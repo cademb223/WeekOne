0ba0ac50ee135bcb64bdcafdce22944f1be7f7c8
Cade Kelly <cademb223@gmail.com>
2019-01-14 18:46:21 -0500

    Added function and some statement

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad just executed!🐳")
     }
 
-
+    @IBAction func showButtonPressed(_ sender: UIButton) {
+    print("🦀🦐🦑🐙I just pressed a button")}
+    
 }
 
